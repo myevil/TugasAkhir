e61fb65954a614a02b9935d281c59dfea8d26230
unknown <williamkarno@gmail.com>
2018-04-24 20:10:10 +0700

    push grayscale

--- ViolaJonesTA/Main.py
+++ ViolaJonesTA/Main.py
@@ -1,7 +1,9 @@
 import glob
 from PIL import Image
-import cv2
-
+from scipy import misc
+import numpy as np
+import matplotlib.pyplot
+import matplotlib.image
 
 def is_Grayscale(width, height, image):
     a = 0
@@ -13,15 +15,8 @@ def is_Grayscale(width, height, image):
     return True
 
 
-def grayscale(width, height, image):
-    img = Image.new(im.mode, im.size)
-    pixelsNew = img.load()
-    for i in range (width):
-        for j in range (height):
-            r, g, b = image.convert('RGB').getpixel((i,j))
-            total = (r + g + b) / 3
-            pixelsNew[i,j] = (int(total),int(total),int(total),255)
-    return image
+def grayscale(image):
+    return np
 
 image_list = []
 for filename in glob.glob('GetImage/*.jpg'):
@@ -30,10 +25,12 @@ for filename in glob.glob('GetImage/*.jpg'):
 
 width, height = image_list[0].size
 
+#print(image_list[0])
+
 print(is_Grayscale(width,height,image_list[0]))
 
 if(is_Grayscale(width,height,image_list[0]) != True):
-    image_list[0] = grayscale(width, height, image_list[0])
+    image_list[0] = rgb2gray(image_list[0])
 
 image_list[0].show()
 
